86a4013c634337b83245f6d1d9a3c821fe1eabff
Will Franklin <will.franklin@guardian.co.uk>
2015-11-30 16:18:24 +0000

    Basic structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,17 @@
+import doT from 'olado/doT'
 import reqwest from 'reqwest'
 import sheetURL from './lib/sheetURL'
 
 import mainHTML from './text/main.html!text'
 
+var templateFn = doT.template(mainHTML);
 var sheet = sheetURL('1pmrlEZalQnqUHYT63y2fsrYbCXtbN1oAkEWHQ_n3VFA', true); // TODO: remove test
 
 function app(el, config, rules) {
+    el.innerHTML =  templateFn({rules, config});
 }
 
 export function init(el, context, config, mediator) {
-    el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
-
     reqwest({
         'url': sheet,
         'type': 'json',
