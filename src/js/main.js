545f8b97b3f3e6f0c71d38ae51d2cee3f01c89bd
Will Franklin <will.franklin@guardian.co.uk>
2015-11-30 16:08:43 +0000

    Sort out assets

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,20 @@
-import iframeMessenger from 'guardian/iframe-messenger'
 import reqwest from 'reqwest'
-import mainHTML from './text/main.html!text'
-import share from './lib/share'
+import sheetURL from './lib/sheetURL'
 
-var shareFn = share('Interactive title', 'http://gu.com/p/URL', '#Interactive');
+import mainHTML from './text/main.html!text'
+
+var sheet = sheetURL('1pmrlEZalQnqUHYT63y2fsrYbCXtbN1oAkEWHQ_n3VFA', true); // TODO: remove test
+
+function app(el, config, rules) {
+}
 
 export function init(el, context, config, mediator) {
-    iframeMessenger.enableAutoResize();
-
     el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
 
     reqwest({
-        url: 'http://ip.jsontest.com/',
-        type: 'json',
-        crossOrigin: true,
-        success: (resp) => el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.ip}`
-    });
-
-    [].slice.apply(el.querySelectorAll('.interactive-share')).forEach(shareEl => {
-        var network = shareEl.getAttribute('data-network');
-        shareEl.addEventListener('click',() => shareFn(network));
+        'url': sheet,
+        'type': 'json',
+        'crossOrigin': true,
+        'success': resp => app(el, config, resp.rules)
     });
 }
