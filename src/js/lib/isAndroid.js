a1e07eb9ecba52f41fa3d3d37ecc34480250842c
Will Franklin <will.franklin@guardian.co.uk>
2015-12-02 15:54:20 +0000

    App hacks

---
+++ src/js/lib/isAndroid.js
@@ -0,0 +1,3 @@
+export default function isAndroid() {
+    return /Android/i.test(navigator.userAgent);
+}
--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,21 @@ import reqwest from 'reqwest'
 import sheetURL from './lib/sheetURL'
 import sticky from './lib/sticky'
 import rAF from './lib/rAF'
+import isAndroid from './lib/isAndroid'
 
 import mainHTML from './text/main.html!text'
+
+const breakpoint = 740;
 
 var templateFn = doT.template(mainHTML);
 var sheet = sheetURL('1pmrlEZalQnqUHYT63y2fsrYbCXtbN1oAkEWHQ_n3VFA', true); // TODO: remove test
 
 function app(el, config, rules) {
-    el.innerHTML =  templateFn({rules, config});
+    el.innerHTML = templateFn({rules, config});
+    el.classList.add('ls-interactive');
+    if (!guardian.api) {
+        el.classList.add('ls-app');
+    }
 
     sticky(el, el.querySelector('.js-ls-container'));
 
@@ -20,10 +27,10 @@ function app(el, config, rules) {
     var mapEls = [].slice.apply(el.querySelectorAll('.js-ls-map'));
 
     var lastMapEl;
-    window.addEventListener('scroll', () => {
+    function setSelected(prop, threshold) {
         rAF(() => {
             for (var i = ruleEls.length - 1; i >= 0; i--) {
-                if (ruleEls[i].getBoundingClientRect().top < 20) {
+                if (ruleEls[i].getBoundingClientRect()[prop] < threshold) {
                     if (mapEls[i] !== lastMapEl) {
                         if (lastMapEl) lastMapEl.classList.remove('is-selected');
                         lastMapEl = mapEls[i];
@@ -37,7 +44,27 @@ function app(el, config, rules) {
                 lastMapEl = undefined;
             }
         });
+    }
+
+    window.addEventListener('scroll', () => {
+        if (window.innerWidth < breakpoint) return;
+        setSelected('top', 20);
     });
+
+    var rulesEl = el.querySelector('.js-ls-rules');
+    rulesEl.addEventListener('scroll', () => {
+        setSelected('left', rulesEl.getBoundingClientRect().left + 20);
+    });
+
+    if (window.GuardianJSInterface && isAndroid()) {
+        rulesEl.addEventListener('touchstart', () => {
+            window.GuardianJSInterface.registerRelatedCardsTouch(true);
+        });
+
+        rulesEl.addEventListener('touchend', () => {
+            window.GuardianJSInterface.registerRelatedCardsTouch(false);
+        });
+    }
 }
 
 export function init(el, context, config, mediator) {
@@ -47,5 +74,4 @@ export function init(el, context, config, mediator) {
         'crossOrigin': true,
         'success': resp => app(el, config, resp.rules)
     });
-
 }
